383e38cc0ac89aa22aa52bcba974ec0562248479
PolazhinetsA <32923196+PolazhinetsA@users.noreply.github.com>
2019-12-06 00:53:38 +0300

    Add files via upload

---
+++ 3d.c
@@ -0,0 +1,297 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <termios.h>
+#include <unistd.h>
+#include "defs.h"
+
+char *screen;
+int wid, hei, widhei;
+
+void tmode_switch(int);
+
+void mainloop();
+
+int main(int argc, char **argv)
+{
+    if (argc == 3) {
+        wid = atoi(argv[1]);
+        hei = atoi(argv[2]);
+    } else {
+        wid = 80;
+        hei = 40;
+    }
+
+    widhei = wid*hei;
+    screen = malloc(widhei);
+
+    tmode_switch(ON);
+    mainloop();
+    tmode_switch(OFF);
+
+    return 0;
+}
+
+const double shift[4][4][3] = { { {-1, 0, 0},
+                                  { 1, 0, 0},
+                                  { 0,-1, 0},
+                                  { 0, 1, 0} },
+
+                                { {-1, 0, 0},
+                                  { 1, 0, 0},
+                                  { 0, 0,-1},
+                                  { 0, 0, 1} },
+
+                                { { 0,-1, 0},
+                                  { 0, 1, 0},
+                                  { 0, 0,-1},
+                                  { 0, 0, 1} } };
+
+
+double center[3] = {0.0, 0.0, 0.0},
+       far = 100.0,
+       zang = 0.0,
+       xang = 0.0;
+
+const char *strview[] = { "Top", "Front", "Side", "3d" };
+
+int view = TOP;
+
+Edges edges;
+
+int adding = 2;
+
+void render();
+void redraw();
+void erase();
+
+void mainloop()
+{
+    erase();
+    redraw();
+
+    edges = EdgesAlloc;
+
+    double last[3] = {};
+
+    for (int key; key = getchar(), key != 'Q' && key != 'q'; )
+    {
+        if (view == _3D)
+        {
+            switch (key) {
+            case 'V': case 'v':
+                view = (view + 1) % 4;
+                break;
+            case 'S': case 's':
+                zang -= 0.1;
+                break;
+            case 'F': case 'f':
+                zang += 0.1;
+                break;
+            case 'D': case 'd':
+                xang -= 0.1;
+                break;
+            case 'E': case 'e':
+                xang += 0.1;
+                break;
+            case '-': case '_':
+                far += 10.0;
+                break;
+            case '=': case '+':
+                far -= 10.0;
+                break;
+            }
+        }
+        else
+        {
+            switch (key) {
+            case 'V': case 'v':
+                view = (view + 1) % 4;
+                break;
+            case 'S': case 's':
+                COMBINE3(center, -=, shift[view][LEFT]);
+                break;
+            case 'F': case 'f':
+                COMBINE3(center, -=, shift[view][RIGHT]);
+                break;
+            case 'D': case 'd':
+                COMBINE3(center, -=, shift[view][DOWN]);
+                break;
+            case 'E': case 'e':
+                COMBINE3(center, -=, shift[view][UP]);
+                break;
+            case 'J': case 'j': if (adding != 2)
+                COMBINE3(EdgesLast(edges)[adding], +=, shift[view][LEFT]);
+                break;
+            case 'L': case 'l': if (adding != 2)
+                COMBINE3(EdgesLast(edges)[adding], +=, shift[view][RIGHT]);
+                break;
+            case 'K': case 'k': if (adding != 2)
+                COMBINE3(EdgesLast(edges)[adding], +=, shift[view][DOWN]);
+                break;
+            case 'I': case 'i': if (adding != 2)
+                COMBINE3(EdgesLast(edges)[adding], +=, shift[view][UP]);
+                break;
+            case ' ':
+                adding = (adding + 1) % 3;
+                switch (adding) {
+                case 0:
+                    {
+                    double newedge[2][3];
+                    COMBINE3(newedge[A], =, last);
+                    COMBINE3(newedge[B], =, last);
+                    EdgesAdd(edges, newedge);
+                    }
+                    break;
+                case 1:
+                    COMBINE3(EdgesLast(edges)[B], =, EdgesLast(edges)[A]);
+                    break;
+                case 2:
+                    COMBINE3(last, =, EdgesLast(edges)[B]);
+                    break;
+                }
+                break;
+            case '-': case '_':
+                far += 10.0;
+                break;
+            case '=': case '+':
+                far -= 10.0;
+                break;
+            }
+        }
+        render();
+        redraw();
+    }
+}
+
+double transform[3][3][3] = { { { 1, 0, 0},
+                                { 0, 1, 0},
+                                { 0, 0, 0} },
+
+                              { { 1, 0, 0},
+                                { 0, 0, 1},
+                                { 0, 0, 0} },
+
+                              { { 0, 1, 0},
+                                { 0, 0, 1},
+                                { 0, 0, 0} } };
+
+double uniform[3][3] = UNIFORM;
+
+void render()
+{
+    double rot1[3][3] = UNIFORM,
+           rot2[3][3] = UNIFORM,
+           (*mat1)[3],
+           (*mat2)[3];
+
+    if (view == _3D)
+    {
+        double zsin = sin(zang),
+               zcos = cos(zang),
+               xsin = sin(xang),
+               xcos = cos(xang);
+
+        rot1[X][X] =  zcos;
+        rot1[X][Y] =  zsin;
+        rot1[Y][X] = -zsin;
+        rot1[Y][Y] =  zcos;
+
+        rot2[Y][Y] =  xcos;
+        rot2[Y][Z] = -xsin;
+        rot2[Z][Y] =  xsin;
+        rot2[Z][Z] =  xcos;
+
+        mat1 = rot1;
+        mat2 = rot2;
+    }
+    else
+    {
+        mat1 = transform[view];
+        mat2 = uniform;
+    }
+
+    erase();
+
+    char (*scr)[wid] = (char (*)[wid])screen;
+
+    for (int i = 0; i < EdgesCount(edges); ++i)
+    {
+        double edge[2][3];
+
+        for (int j = 0; j < 2; ++j)
+        {
+            COMBINE3(edge[j], =, edges[i][j]);
+            COMBINE3(edge[j], -=, center);
+
+            VECMAT3(edge[j], mat1);
+            VECMAT3(edge[j], mat2);
+
+            edge[j][Z] += far;
+            if (edge[j][Z] < 10.0) continue;
+
+            edge[j][X] = edge[j][X]*100.0/edge[j][Z];
+            edge[j][Y] = edge[j][Y]*100.0/edge[j][Z];
+        }
+
+        if (adding != 2 && &edges[i] == &EdgesLast(edges))
+        {
+            for (int j = 0; j <= adding; ++j)
+            {
+                if (edge[j][Z] < 10.0) continue;
+
+                int col = wid/2 + edge[j][X]*2,
+                    row = hei/2 - edge[j][Y];
+
+                if (0 <= col && col < wid-1 && 0 <= row && row < hei)
+                    scr[row][col] = 'A'+j;
+            }
+        }
+        else if (edge[A][Z] >= 10.0 && edge[B][Z] >= 10.0)
+        {
+            double len = fmax(fabs(edge[A][X]-edge[B][X]),
+                              fabs(edge[A][Y]-edge[B][Y]));
+
+            for (double l = 0; l <= len; l += 1.0)
+            {
+                int col, row;
+                col = wid/2 + (edge[A][X] + (edge[B][X]-edge[A][X])*l/len)*2,
+                row = hei/2 - (edge[A][Y] + (edge[B][Y]-edge[A][Y])*l/len);
+
+                if (0 <= col && col < wid-1 && 0 <= row && row < hei)
+                    scr[row][col] = '#';
+            }
+        }
+    }
+}
+
+void redraw()
+{
+    fwrite(screen, 1, widhei, stdout);
+    printf("%-5s \n\e[%dA", strview[view], hei+1);
+}
+
+void erase()
+{
+    memset(screen, ' ', widhei);
+    for (int y = 1; y <= hei; ++y) screen[y*wid-1] = '\n';
+}
+
+void tmode_switch(int on_off)
+{
+    static struct termios tmode_old, tmode_new;
+
+    switch (on_off) {
+    case ON:
+        tcgetattr(STDIN_FILENO, &tmode_old);
+        tmode_new = tmode_old;
+        tmode_new.c_lflag &= ~(ICANON);
+        tmode_new.c_lflag &= ~(ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &tmode_new);
+        break;
+    case OFF:
+        tcsetattr(STDIN_FILENO, TCSANOW, &tmode_old);
+        break;
+    }
+}
---
+++ defs.h
@@ -0,0 +1,71 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#include <stdlib.h>
+#include <stdint.h>
+
+enum { TOP, FRONT, SIDE, _3D };
+enum { LEFT, RIGHT, DOWN, UP };
+enum { X, Y, Z };
+enum { A, B };
+enum { OFF, ON };
+
+#define UNIFORM { { 1, 0, 0}, \
+                  { 0, 1, 0}, \
+                  { 0, 0, 1} }
+
+#define COMBINE3(dst, op, src)  ( dst[0] op src[0], \
+                                  dst[1] op src[1], \
+                                  dst[2] op src[2] )
+
+typedef double (*Edges)[2][3];
+
+#define SZREALLOC 0x100
+
+#define EdgesCount(edges) \
+    (((uint32_t *)edges)[-1])
+
+#define EdgesLast(edges) \
+    (edges[EdgesCount(edges)-1])
+
+#define EdgesAlloc \
+    ((Edges )((uint32_t *)calloc(1, sizeof(uint32_t)) + 1))
+
+#define EdgesRealloc(edges,sz) \
+    ((Edges )((uint32_t *)realloc(&EdgesCount(edges), sz) + 1))
+
+#define EdgesFree(edges) \
+    (free(&EdgesCount(edges)))
+
+#define EdgesAdd(edges, edge) {                                             \
+    if (EdgesCount(edges) % SZREALLOC == 0) {                               \
+        size_t _sz = sizeof(uint32_t) + SZREALLOC*sizeof(*(Edges )NULL);    \
+        edges = EdgesRealloc(edges, _sz);                                   \
+    }                                                                       \
+    COMBINE3(edges[EdgesCount(edges)][A], =, edge[A]);                      \
+    COMBINE3(edges[EdgesCount(edges)][B], =, edge[B]);                      \
+    ++EdgesCount(edges);                                                    \
+}
+
+#define VECMAT3(dst, src)       \
+{                               \
+    typeof(*dst) _0, _1, _2;    \
+                                \
+    _0 = dst[0]*src[0][0]       \
+       + dst[1]*src[0][1]       \
+       + dst[2]*src[0][2];      \
+                                \
+    _1 = dst[0]*src[1][0]       \
+       + dst[1]*src[1][1]       \
+       + dst[2]*src[1][2];      \
+                                \
+    _2 = dst[0]*src[2][0]       \
+       + dst[1]*src[2][1]       \
+       + dst[2]*src[2][2];      \
+                                \
+    dst[0] = _0;                \
+    dst[1] = _1;                \
+    dst[2] = _2;                \
+}
+
+#endif
